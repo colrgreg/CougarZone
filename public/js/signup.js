b1f27f4f8d495d368c478100b716b6529a2319ee
bclinchuck <bclinchuck@gmail.com>
2025-11-02 18:42:32 -0500

    Update signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,16 +1,15 @@
 console.log("Signup JS loaded!"); 
 
-import { auth } from "./firebase.js";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+const signupForm = document.getElementById("signupForm");
 
-document.getElementById("signupForm").addEventListener("submit", async (e) => {
+signupForm.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
     try {
-        await createUserWithEmailAndPassword(auth, email, password);
+        await firebase.auth().createUserWithEmailAndPassword(firebase.auth(), email, password);
         alert("Account created!");
         window.location.href = "/login";
     } catch (err) {
